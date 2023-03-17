4137491d0b01305629f4ddc5c8a7e7a75db48874
Aamir Azad <82281117+aamirazad@users.noreply.github.com>
2023-03-16 20:39:39 -0400

    Add DNA flashcardsgit add svelte/*

---
+++ svelte/app/src/routes/flash/DNA/vocabData.js
@@ -0,0 +1,111 @@
+export const vocab = [
+    {
+        "clue": "DNA structure",
+        "word": "Deoxyribose (sugar), a phosphate group, and a nitrogenous base"
+    },
+    {
+        "clue": "What makes up the \"backbone\"",
+        "word": "Deoxyribose and phosphates"
+    },
+    {
+        "clue": "What makes up the \"rungs\"",
+        "word": "Nitrogenous bases"
+    },
+    {
+        "clue": "Adenine pairs with _____, while guanine pairs up with _____",
+        "word": "Thymine, Cytosine"
+    },
+    {
+        "clue": "Type of bond that holds the bases",
+        "word": "Hydrogen bonds"
+    },
+    {
+        "clue": "DNA sequences that code for genes",
+        "word": "Exons"
+    },
+    {
+        "clue": "DNA sequences that don't code for genes",
+        "word": "Introns"
+    },
+    {
+        "clue": "The process in which DNA is copied",
+        "word": "Replication"
+    },
+    {
+        "clue": "Enzymes are involved with DNA replication",
+        "word": "DNA helicase, DNA polymerase, DNA ligase"
+    },
+    {
+        "clue": "DNA helicase, polymerase, and ligase's function'",
+        "word": "DNA Helicase unzips, polymerase proofreads, and ligase stitches DNA back together"
+    },
+    {
+        "clue": "What is the sugar in DNA and in RNA",
+        "word": "Ribose in RNA and deoxyribose for DNA"
+    },
+    {
+        "clue": "Type of RNA that carries protein assembly instructions",
+        "word": "mRNA"
+    },
+    {
+        "clue": "Type of RNA that regulates translation at the ribosome",
+        "word": "rRNA"
+    },
+    {
+        "clue": "RNA that transfers amino acids that connect to make proteins",
+        "word": "tRNA"
+    },
+    {
+        "clue": "The process of producing RNA from DNA",
+        "word": "Transcription"
+    },
+    {
+        "clue": "Enzyme is used in transcription",
+        "word": "RNA polymerase"
+    },
+    {
+        "clue": "Location of transcription",
+        "word": "In the nucleus for eukaryotes and in the cytoplasm for prokaryotes"
+    },
+    {
+        "clue": "The process of making a protein from RNA",
+        "word": "Translation"
+    },
+    {
+        "clue": "Something that causes genetic mutation",
+        "word": "Mutagen"
+    },
+    {
+        "clue": "Gene mutation that replaces a base for another",
+        "word": "Substitution"
+    },
+    {
+        "clue": "Gene mutation where a additional base is added",
+        "word": "Insertion"
+    },
+    {
+        "clue": "Gene mutation where a base is removed",
+        "word": "Deletion"
+    },
+    {
+        "clue": "Gene mutation that causes a stop protein",
+        "word": "Nonsense"
+    },
+    {
+        "clue": "Mutation that does not change the amino acid produced",
+        "word": "Silent mutation"
+    },
+    {
+        "clue": "Chromosome mutation that results in the removal of a part of a chromosome",
+        "word": "Deletion"
+    },
+    {
+        "clue": "Chromosomal mutation where extra copies of a chromosome is produced",
+        "word": "Duplication"
+    },
+    {
+        "clue": "Chromosomal mutation where the direction of parts of a chromosome is reversed",
+        "word": "Inversion"
+    }
+];
+
